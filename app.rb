e7c0209a00e2cf496982c723c3201b39c4f03cd6
Pablo Cantero <pablo@pablocantero.com>
2012-09-18 21:04:41 -0300

    Adds animated Hello World

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require "sinatra"
 
 get "/*" do
-    "Hello World!"
+  # sleep 60 * 4
+  erb :hello
 end
+
+__END__
+@@ layout
+<html>
+  <head>
+    <title>Hello World!</title>
+    <style>
+      h1 {
+        font-size: 12em;
+      }
+    </style>
+  </head>
+  <body>
+    <%= yield %>
+  </body>
+</html>
+
+@@ hello
+<marquee><h1>Hello World!</h1></marquee>
